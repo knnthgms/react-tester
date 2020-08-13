fe07261555ae2f19a0a8b64c1075d90dac21dba3
kengomes <knnthgms@gmail.com>
2020-08-13 11:54:17 +0100

    Added filter

--- src/components/AnimalFarm/AnimalFarm.js
+++ src/components/AnimalFarm/AnimalFarm.js
@@ -20,8 +20,20 @@ class AnimalFarm extends React.Component {
     this.setState({ userChoice });
   };
 
+  selectType = animalType => {
+    const { animalList } = this.state;
+    const searchedAnimals = animalList.animals.filter(
+      a => a.type === animalType
+    );
+    this.setState({ searchedAnimals });
+  };
+
+  clearSearch = () => {
+    this.setState({ searchedAnimals: null });
+  };
+
   render() {
-    const { fetchingData, animalList } = this.state;
+    const { fetchingData, animalList, searchedAnimals } = this.state;
     const allTypesList = animalList && [
       ...new Set(animalList.animals.map(e => e.type))
     ];
@@ -33,11 +45,36 @@ class AnimalFarm extends React.Component {
           <React.Fragment>
             <DropMenu onSelect={this.getUserChoice} />
             <span>Types are</span>
-            {allTypesList.map((a, i) => (
-              <span key={i} className="tag">
-                {a}
-              </span>
-            ))}
+            <div className="types-list">
+              {allTypesList.map((a, i) => (
+                <button
+                  key={i}
+                  className="tag"
+                  onClick={() => this.selectType(a)}
+                >
+                  {a}
+                </button>
+              ))}
+            </div>
+            <div className="animal-cards">
+              {searchedAnimals && (
+                <button onClick={this.clearSearch}>Clear Search</button>
+              )}
+              {searchedAnimals &&
+                searchedAnimals.map((ele, index) => {
+                  return (
+                    <React.Fragment>
+                      <AnimalCard
+                        key={index}
+                        name={ele.name}
+                        type={ele.type}
+                        age={ele.age}
+                        sex={ele.sex}
+                      />
+                    </React.Fragment>
+                  );
+                })}
+            </div>
             <div className="animal-cards">
               {animalList.animals.map((ele, index) => {
                 return (
