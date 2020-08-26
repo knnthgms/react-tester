e5c6967a6fdf32fd701da0c5093877d9469ea600
kengomes <knnthgms@gmail.com>
2020-08-26 09:12:52 +0100

    hooks

--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
 import { render } from "react-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AnimalFarm from "./components/AnimalFarm";
 import Api from "./Api";
 import "./index.scss";
 
-class App extends React.Component {
-  state = {
-    fetchingData: true,
-    fetchingError: false,
-    data: null,
-  };
-  componentDidMount() {
-    this.loadData();
-  }
-  loadData = async () => {
-    this.setState({ fetchingData: true });
-    const response = await Api.getAnimalData();
-    const data = response.result;
-    const status = response.status;
-    this.setState({
-      data,
-      fetchingData: false,
-      fetchingError: !data || status !== "ok",
-    });
-  };
-  render() {
-    const { data, fetchingData, fetchingError } = this.state;
-    const renderCondition = data || !fetchingData || fetchingError;
-    return (
-      <div className="main-app">
-        <header className="app-header">
-          <span className="header-text">Leaf Grow coding assessment</span>
-        </header>
-        <main className="app-body">
-          {renderCondition && (
-            <AnimalFarm
-              data={data}
-              fetchingData={fetchingData}
-              fetchingError={fetchingError}
-            />
-          )}
-        </main>
-        <footer className="app-footer">{new Date().toDateString()}</footer>
-      </div>
-    );
-  }
+function App() {
+  const [fetchingData, setLoading] = useState(true);
+  const [fetchingError, setError] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      const response = await Api.getAnimalData();
+      setData(response.result);
+      setError(!response.data || response.status !== "ok");
+      setLoading(false);
+    };
+    loadData();
+  }, []);
+
+  return (
+    <div className="main-app">
+      <header className="app-header">
+        <span className="header-text">Leaf Grow coding assessment</span>
+      </header>
+      <main className="app-body">
+        {(data || !fetchingData || fetchingError) && (
+          <AnimalFarm
+            data={data}
+            fetchingData={fetchingData}
+            fetchingError={fetchingError}
+          />
+        )}
+      </main>
+      <footer className="app-footer">{new Date().toDateString()}</footer>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById("root"));
